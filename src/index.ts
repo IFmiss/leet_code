89dcbe55eb725c033412def7f249aa5ca4784207
ifmiss <185098535@qq.com>
2021-01-06 08:50:34 +0800

    update: cy

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import LinkedList, { linkToArray } from './leetcode/linkedList'
 import generate from './leetcode/_118'
 import getRow from './leetcode/_119'
 import fib from './leetcode/_509';
+import { climbStairs } from './leetcode/_70';
+import { add, fact, fact1 } from './leetcode/weidigui';
 
 console.log(reverseString(["h","e","l","l","o"]))
 console.log(reverseString(["H","a","n","n","a","h"]))
@@ -19,6 +21,11 @@ link.insert(4, 3)
 
 
 linkToArray(swapPairs(link.head?.next || null))
+
+// console.info('climbStairs', climbStairs(40))
+// console.info('fact1', fact1(6, 1));
+// console.info('fact', fact(6));
+console.info('add', add(6, 5));
 
 // console.log(generate(5))
 console.log('getRow(4)', getRow(3))
