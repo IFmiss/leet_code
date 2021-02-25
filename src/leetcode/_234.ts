21492576246a9275e9c382c48fe8c8f0da4fc479
ifmiss <185098535@qq.com>
2021-02-25 08:57:30 +0800

    update: cy

--- src/leetcode/_234.ts
+++ src/leetcode/_234.ts
@@ -10,3 +10,50 @@
 // 输出: true
 // 进阶：
 // 你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题
+
+// function isPalindrome(head: ListNode | null): boolean {
+//   if (!head) return false;
+//   let stack = [];
+//   let posHead = head;
+//   while(posHead) {
+//     stack.push(posHead.val);
+//     posHead = posHead.next;
+//   }
+
+//   if (stack.length === 1) return true;
+
+//   while(stack.length) {
+//     if (stack.pop() === head.val) {
+//       head = head.next;
+//     }
+//     return false;
+//   }
+//   return true;
+// };
+
+function isPalindrome(head: ListNode | null): boolean {
+  if (!head) return false;
+  let slow = head;
+  let fast = head;
+  let pre = null;
+  let reverse = null;
+  while(fast && fast.next) {
+    pre = slow;
+    slow = slow.next;
+    fast = fast.next.next;
+    pre.next = reverse;
+    reverse = pre;
+  }
+  if (fast) {
+    slow = slow.next;
+  }
+
+  while(slow) {
+    if (slow.val !== reverse.val) {
+      return false;
+    }
+    slow = slow.next;
+    reverse = reverse?.next;
+  }
+  return true;
+}
